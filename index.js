23eff1c618f3bc1f8a2e99378da3c5bb3ec7e6bb
Jannis R <mail@jannisr.de>
2018-05-03 18:08:19 +0200

    implementation

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 'use strict'
 
-// todo
+const mapping = require('merged-vbb-stations')
+const stations = require('vbb-stations/full.json')
+
+const stationOf = Object.create(null)
+
+for (let origId in stations) {
+	const newId = mapping[origId]
+
+	stationOf[origId] = newId
+	for (let stop of stations[origId].stops) stationOf[stop.id] = newId
+}
+
+const create = (allVariants) => {
+	const stationCounts = Object.create(null)
+	for (let v of allVariants) {
+		for (let stop of v.stations) {
+			const station = stationOf[stop]
+			if (!stationCounts[station]) stationCounts[station] = 1
+			else stationCounts[station]++
+		}
+	}
+
+	const computeLineVariantScore = (variant) => {
+		let score = 0
+		for (let stop of variant.stations) {
+			const station = stationOf[stop]
+			score += stationCounts[station]
+		}
+		return score * variant.trips
+	}
+	return computeLineVariantScore
+}
+
+module.exports = create
