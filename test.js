9a74c0d17b29124d31fb38483ad9ff12b8c5301d
Jannis R <mail@jannisr.de>
2018-05-05 12:22:46 +0200

    U2 test :white_check_mark:

--- test.js
+++ test.js
@@ -15,45 +15,66 @@ for (let oldId in stations) {
 	for (let stop of stations[oldId].stops) stationOf[stop.id] = newId
 }
 
-const getU2 = (cb) => {
+const getBestVariants = (name, cb) => {
 	const s = readLines()
 	s.on('data', (line) => {
-		if (line.name.toLowerCase() === 'u2') {
+		if (line.name.toLowerCase() === name.toLowerCase()) {
 			s.destroy()
-			cb(line)
+
+			const computeLineVariantScore = create(line.variants)
+			const rank = hifo(hifo.highest('score'), 2)
+			for (let variant of line.variants) {
+				rank.add({
+					score: computeLineVariantScore(variant),
+					stops: variant.stops
+				})
+			}
+			cb(rank.data)
 		}
 	})
 }
 
 const ruhleben = '900000025202'
 const pankow = '900000130002'
+const uhlandstr = '900000023301'
+const warschauerStr = '900000120004'
 
 test('works with U2', (t) => {
 	t.plan(6)
-
-	getU2((line) => {
-		const computeLineVariantScore = create(line.variants)
-		const rank = hifo(hifo.highest('score'), 2)
-
-		for (let variant of line.variants) {
-			rank.add({
-				score: computeLineVariantScore(variant),
-				stations: variant.stations
-			})
-		}
-
-		const a = rank.data.find(v => stationOf[v.stations[0]] === ruhleben)
+	getBestVariants('U2', (variants) => {
+		const a = variants.find(v => stationOf[v.stops[0]] === ruhleben)
 		t.ok(a)
 		if (a) {
 			t.ok(a.score > 0)
-			t.equal(stationOf[a.stations[a.stations.length - 1]], pankow)
+			t.equal(stationOf[a.stops[a.stops.length - 1]], pankow)
 		}
 
-		const b = rank.data.find(v => stationOf[v.stations[0]] === pankow)
+		const b = variants.find(v => stationOf[v.stops[0]] === pankow)
 		t.ok(b)
 		if (b) {
 			t.ok(b.score > 0)
-			t.equal(stationOf[b.stations[b.stations.length - 1]], ruhleben)
+			t.equal(stationOf[b.stops[b.stops.length - 1]], ruhleben)
+		}
+
+		t.end()
+	})
+})
+
+test('works with U1', (t) => {
+	t.plan(6)
+	getBestVariants('U1', (variants) => {
+		const a = variants.find(v => stationOf[v.stops[0]] === uhlandstr)
+		t.ok(a)
+		if (a) {
+			t.ok(a.score > 0)
+			t.equal(stationOf[a.stops[a.stops.length - 1]], warschauerStr)
+		}
+
+		const b = variants.find(v => stationOf[v.stops[0]] === warschauerStr)
+		t.ok(b)
+		if (b) {
+			t.ok(b.score > 0)
+			t.equal(stationOf[b.stops[b.stops.length - 1]], uhlandstr)
 		}
 
 		t.end()
